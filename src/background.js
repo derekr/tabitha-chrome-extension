f3ff2275164778f27e0a90ba3e095ec59df35dd6
Derek <derekr@me.com>
2015-05-05 10:31:18 -0400

    Big build

--- src/background.js
+++ src/background.js
@@ -1,4 +1,5 @@
 var nets = require('nets')
+var url = require('url')
 
 var count = 0
 function update (e) {
@@ -17,13 +18,59 @@ function update (e) {
         })
     }
 
-    console.dir(opts)
+    nets(opts, function (err, res) {
+        if (err) return console.error(err)
+    })
+}
+
+function remove (e) {
+    count -= 1
+    if (count < 0) return
+
+    chrome.browserAction.setBadgeText({text: count + ''});
+
+    var opts = {
+        url: process.env.BASE_URL + '/api/tab',
+        method: 'DELETE',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        encoding: undefined,
+        body: JSON.stringify({
+            id: 'hey'
+        })
+    }
 
     nets(opts, function (err, res) {
         if (err) return console.error(err)
-
-        console.dir(res)
     })
 }
+
+function loaded (id, data) {
+    if (data.status !== 'loading') return
+
+    var _host = url.parse(data.url).hostname
+
+    if (['devtools', 'newtab'].indexOf(_host) > -1) return
+
+    var opts = {
+        url: process.env.BASE_URL + '/api/url',
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        encoding: undefined,
+        body: JSON.stringify({
+            url: _host
+        })
+    }
+
+    nets(opts, function (err, res) {
+        if (err) return console.error(err)
+    })
+}
+
 chrome.runtime.onInstalled.addListener(update)
 chrome.tabs.onCreated.addListener(update)
+chrome.tabs.onRemoved.addListener(remove)
+chrome.tabs.onUpdated.addListener(loaded)
